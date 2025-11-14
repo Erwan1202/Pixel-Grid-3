6eb5f5b2dbc0b09b6893cdc93c944cf40eab70fa
Erwan <erwan.a.marechal@gmail.com>
2025-11-14 23:25:42 +0100

    feat(database): Setup connections for PostgreSQL (pg) and MongoDB (mongoose)

---
+++ src/models/db.postgres.js
@@ -0,0 +1,23 @@
+const { Pool } = require('pg');
+
+const pool = new Pool({
+    connectionString: process.env.PG_URI,
+    ssl: {
+        rejectUnauthorized: false,
+    },
+});
+
+const execute = async (query, params = []) => {
+    try {
+        const client = await pool.connect();
+        const result = await client.query(query, params);
+        client.release();
+        return result;
+    } catch (error) {
+        throw error;
+    }
+};
+
+module.exports = {
+    execute,
+};
