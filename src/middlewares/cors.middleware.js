c2398d53e70f8e9ee9e583f418f89c2f124b1897
Erwan <erwan.a.marechal@gmail.com>
2025-11-15 12:33:51 +0100

    refactor(cors.middleware): enhance CORS handling and restructure middleware integration

--- src/middlewares/cors.middleware.js
+++ src/middlewares/cors.middleware.js
@@ -1,23 +1,58 @@
-const cors = require('cors');
+require('dotenv').config();
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+
+const corsMiddleware = require('./src/middlewares/cors.middleware');
+const { connectDB } = require('./src/models/db.connect');
+const authRoutes = require('./src/routes/auth.routes');
+const gridRoutes = require('./src/routes/grid.routes');
+
+const app = express();
+const port = process.env.PORT || 3001;
+const server = http.createServer(app);
 
 const allowedOrigins = [
-    process.env.FRONTEND_URL, 
+    process.env.FRONTEND_URL,
     'http://localhost:5173'
 ];
+const VERCEL_PREVIEW_REGEX = /^https:\/\/pixel-grid-3-.*\\.vercel\\.app$/;
 
-const VERCEL_PREVIEW_REGEX = /^https:\/\/pixel-grid-3-.*\.vercel\.app$/;
-
-const corsOptions = {
-    origin: (origin, callback) => {
-        if (allowedOrigins.indexOf(origin) !== -1 || VERCEL_PREVIEW_REGEX.test(origin) || !origin) {
-            callback(null, true);
-        } else {
-            console.error('CORS blocked origin:', origin);
-            callback(new Error('Not allowed by CORS'));
-        }
-    },
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true,
+const corsOriginLogic = (origin, callback) => {
+    if (allowedOrigins.indexOf(origin) !== -1 || VERCEL_PREVIEW_REGEX.test(origin) || !origin) {
+        callback(null, true);
+    } else {
+        console.error('CORS blocked origin:', origin);
+        callback(new Error('Not allowed by CORS'));
+    }
 };
 
-module.exports = cors(corsOptions);
+const io = new Server(server, {
+    cors: {
+        origin: corsOriginLogic,
+        methods: ['GET', 'POST']
+    }
+});
+
+app.use(cors({
+    origin: corsOriginLogic,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true
+}));
+
+app.use(express.json());
+
+app.use((req, res, next) => {
+    req.io = io;
+    next();
+});
+
+connectDB();
+
+app.use('/api/auth', authRoutes);
+app.use('/api/grid', gridRoutes);
+
+app.use((err, req, res, next) => {
+});
+
+module.exports = server;
